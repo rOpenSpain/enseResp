5504478e9434bf2e70cadcbc74263c474fcc6a12
edugonzaloalmorox <eduardogonzaloalmorox@gmail.com>
2020-09-27 22:29:40 +0100

    Add labels dataset

--- R/children_19_labels.R
+++ R/children_19_labels.R
@@ -2,9 +2,9 @@
 #' @description Provides labels to the values of the children survey. ENSE 17/19
 #' @format A data frame with 1310 rows and 3 variables:
 #' \describe{
-#'   \item{\code{valores_ine}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{valores}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{variable_ine}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{valores_ine}}{values of the variable}
+#'   \item{\code{valores}}{labels}
+#'   \item{\code{variable_ine}}{name of the variable}
 #'}
 #' @details DETAILS
 "children_19_labels"
--- data-raw/household_data.R
+++ data-raw/household_data.R
@@ -1,24 +1,64 @@
-## code to prepare `household_19`  and household_19_info dataset goes here
+## code to prepare `household_19`, household_19_info and household_19_label, dataset goes here
 
 library(tidyverse)
 library(readxl)
 library(janitor)
 
+# Functions ------------------------------------------------
 
-info_household <- read_excel("Hogar_ENSE17/Diseno registro HOGAR ENSE 2017_PUBLICACION.xlsx", range = "A9:E99")  %>%
+house_info = function(df, filter_var){
+
+  df %>%
+    filter(!is.na({{ filter_var }})) %>%
+    mutate(tipo_variable =  ifelse(is.na(longitud), {{ filter_var }}, NA)) %>%
+    fill(tipo_variable, .direction = "down") %>%
+    filter(!is.na(longitud), longitud != "LONGITUD") %>%
+    mutate_at(vars(posicion_inicio, posicion_final, longitud), list(as.numeric))
+
+
+}
+
+
+house_labels = function(df){
+
+  x = df %>%
+    rename(var1 = `...1`,
+           var2 = `...2`,
+           var3 = `...3`) %>%
+    mutate(var4  = ifelse(var1 == "Variable" | var1 == "Variable:" & !is.na(var2), var2, NA),
+           var4 = ifelse(var2 == "CLASE_PR", "CLASE_PR", var4 )) %>%
+    fill(var4, .direction = "down") %>%
+    filter(!is.na(var2) & var1 == "Valores"  | var1 == "Valores:"  | is.na(var1)) %>%
+    mutate(var5 = ifelse(is.na(var1) & is.na(var2) & is.na(var3) & !is.na(var4), TRUE, FALSE )) %>%
+    filter(var5 == FALSE) %>%
+    select(valores_ine = var2, valores = var3, variable_ine = var4)
+
+  return(x)
+
+}
+
+
+
+
+# Load raw data -----------------------------------------
+
+# ENSE 2017
+
+
+household_17 <- read_excel("Hogar_ENSE17/Diseno registro HOGAR ENSE 2017_PUBLICACION.xlsx", range = "A9:E99")  %>%
   janitor::clean_names()
 
-
-household_info_final = info_household %>%
-  filter(!is.na(variable_ine), variable_ine != "VARIABLE INE") %>%
-  mutate(tipo_variable = ifelse(is.na(longitud), variable_ine, NA)) %>%
-  fill(tipo_variable, .direction = "down") %>%
-  filter(!is.na(longitud), longitud != "LONGITUD") %>%
-  mutate(tipo_variable = ifelse(is.na(tipo_variable), "PROTOCOLO DE LA ENTREVISTA", tipo_variable))
+house_17_labels = read_excel("Hogar_ENSE17/Diseno registro HOGAR ENSE 2017_PUBLICACION.xlsx", sheet = 2, range = "A8:C467")
 
 
-household_19_info = household_info_final %>%
-  mutate_at(vars(posicion_inicio, posicion_final, longitud), list(as.numeric))
+
+# Information
+
+household_19_info = house_info(household_17, variable_ine)
+
+household_19_labels = house_labels(house_17_labels)
+
+
 
 
 # Load microdata
@@ -28,4 +68,4 @@ household_19 = readr::read_fwf(file = "Hogar_ENSE17/MICRODAT.CH.txt",
                               #fwf_widths(adults_info_final$longitud, adults_info_final$variable_ine),
                               fwf_positions(household_19_info$posicion_inicio, household_19_info$posicion_final, household_19_info$variable_ine))
 
-usethis::use_data(household_19, household_19_info, overwrite = T)
+usethis::use_data( household_19, household_19_info, household_19_labels, overwrite = T)
