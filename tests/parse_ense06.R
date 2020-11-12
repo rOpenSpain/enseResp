a9c0b3fef9cdc2211d954727a373e739301fcbcd
edugonzaloalmorox <eduardogonzaloalmorox@gmail.com>
2020-11-12 21:59:39 +0000

    Parse pdf info adults 2006

--- tests/parse_ense06.R
+++ tests/parse_ense06.R
@@ -66,4 +66,93 @@ info_adults_clean = bind_rows(bit1, bit2, bit3, bit4) %>% select(-row_n)
 
 write_csv(info_adults_clean, "documentation/ense06/Adulto-ENSE-2006/variables_registro_adultos_2006.csv")
 
+# Parse pdf
+
+library(pdftools)
+
+registro = pdftools::pdf_text(pdf = "documentation/ense06/Adulto-ENSE-2006/Registro Adultos 2006.pdf")
+
+registro_clean = strsplit(registro, "\n")
+
+test = registro_clean %>% unlist() %>% tibble()
+
+colnames(test) = "var"
+
+
+test2 = test %>%
+  slice(636:5627) %>%
+  mutate(variable = ifelse(str_detect(var, "^??? NOMBRE DE LA VARIABLE"), var,
+                           ifelse(str_detect(var, "CONTENIDO = "), var,
+                                  ifelse(str_detect(var, "LONGITUD = "), var,
+                                         ifelse(str_detect(var, "VALORES Y SIGNIFICADO"), var, NA))))) %>%
+  mutate(var_clean = ifelse(str_detect(var, "^??? NOMBRE DE LA VARIABLE"), var, NA),
+         var_clean = gsub("??? NOMBRE DE LA VARIABLE = ", "", var_clean)) %>%
+  fill(var_clean, .direction = "down")
+
+
+
+test3 = test2 %>%
+  mutate(check = ifelse(str_detect(variable, "CONTENIDO = ") & is.na(lead(variable)), TRUE, FALSE),
+         check2 = ifelse(lag(check) == TRUE, "Y", NA),
+         cont_clean = ifelse(check == TRUE, paste(variable, lead(var), sep = " "), NA)) %>%
+  filter(is.na(check2)) %>%
+  select(-check, -check2) %>%
+  mutate(type_var = ifelse(str_detect(var, "^??? NOMBRE DE LA VARIABLE"), "nombre",
+                           ifelse(str_detect(var, "CONTENIDO = "), "contenido",
+                                  ifelse(str_detect(var, "LONGITUD = "), "longitud",
+                                         ifelse(str_detect(var, "VALORES Y SIGNIFICADO"), "valores", NA))))) %>%
+  fill(type_var, .direction = "down") %>%
+  mutate(cont_clean = ifelse(is.na(cont_clean), var, cont_clean),
+         cont_clean = str_remove_all(cont_clean, paste(c("??? NOMBRE DE LA VARIABLE = ",
+                                                         "CONTENIDO = ",
+                                                         "LONGITUD = ",
+                                                         "VALORES Y SIGNIFICADO"), collapse = "|")),
+         cont_clean = ifelse(type_var != "nombre", str_to_sentence(cont_clean), cont_clean))
+
+
+
+test4 = test3 %>%
+  select(var_clean, type_var, cont_clean) %>%
+  filter(type_var != "valores") %>%
+  pivot_wider(names_from = type_var, values_from = cont_clean) %>%
+  select(nombre, contenido, longitud) %>%
+  unnest(c(nombre, contenido, longitud))
+
+test4
+
+
+# Get the variable
+x = read_csv( "documentation/ense06/Adulto-ENSE-2006/variables_registro_adultos_2006.csv")
+setdiff(unique(x$variable), unique(test4$nombre))
+setdiff(unique(test4$nombre), unique(x$variable))
+
+
+
+x = x %>%
+  mutate(variable = gsub("K", "J", variable),
+         variable = ifelse(variable == "PROV *", "PROV",
+                           ifelse(variable == "TRIM *", "TRIM", variable)))
+
+setdiff(unique(x$variable), unique(test4$nombre))
+
+
+
+
+prueba = test4 %>%
+  select(-longitud) %>%
+  full_join(., x , by = c("nombre" = "variable")) %>%
+  arrange(posicion_inicio) %>%
+  mutate(contenido = ifelse(posicion_inicio == 850 & lead(contenido) == "Sea ocupado)",
+                            paste(contenido, lead(contenido), sep = " "), contenido)) %>%
+  filter(contenido != "Sea ocupado)") %>%
+  mutate(contenido = str_to_sentence(contenido))
+
+
+
+prueba = prueba %>%
+  select(campo = nombre,
+         longitud, posicion_inicio, posicion_final = posicion_fin,
+         descripcion_del_campo = contenido, valores_validos)
+
+write_csv(prueba, "documentation/ense06/Adulto-ENSE-2006/adultos_2006_info.csv")
 
